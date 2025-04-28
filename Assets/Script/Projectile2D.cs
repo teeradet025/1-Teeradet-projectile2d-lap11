9ed0cfa5538e4c959f31e4035ef541eb258dcf6a
teeradet025 <201479094+teeradet025@users.noreply.github.com>
2025-04-28 12:58:04 +0700

    0.3

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -9,7 +9,21 @@ public class Projectile2D : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f , Color.red , 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+            
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit" + hit.collider.name);
+
+                
+            }
+        }
     }
 
 }
